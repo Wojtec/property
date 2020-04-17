ff5c3fa2e9898d644af1836d42e98f2865ce54ad
Wojtec <poninski.w@gmail.com>
2020-04-18 01:35:58 +0200

    “settin”

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,4 @@
-// dotenv config
-require('dotenv').config();
 
 module.exports = {
-    url: "mongodb+srv://property:property@cluster0-0gkej.mongodb.net/test?retryWrites=true&w=majority"
+    url: process.env.DB_CONNECTION
 }
