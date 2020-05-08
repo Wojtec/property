a8d019f9786c35e6e55a1c216a4bc6af0c440d81
Wojtec <poninski.w@gmail.com>
2020-05-08 20:55:04 +0200

    anothet

--- api.js
+++ api.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 //make public uplad folder
 
-app.use(express.static(__dirname, '/public'));
+app.use(express.static(__dirname + '/public'));
 //connection to Mongoose and set connection variable
 mongoose.connect(dbUrl.url,{
    useNewUrlParser: true,
