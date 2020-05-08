a1f9d3623c147aa2c178c3ec2acee17b9c817952
Wojtec <poninski.w@gmail.com>
2020-05-08 20:11:20 +0200

    join

--- api.js
+++ api.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json());
 //make public uplad folder
-app.use('/img', express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 console.log(__dirname+ "/public");
 //connection to Mongoose and set connection variable
 mongoose.connect(dbUrl.url,{
