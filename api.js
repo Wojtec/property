7069c65cd7178afe8f7335857f9b535560994109
Wojtec <poninski.w@gmail.com>
2020-05-08 19:25:48 +0200

    another app

--- api.js
+++ api.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json());
 //make public uplad folder
-app.use(express.static('app/public/upload'));
+app.use('/img', express.static('./app/public/upload'));
 //connection to Mongoose and set connection variable
 mongoose.connect(dbUrl.url,{
    useNewUrlParser: true,
